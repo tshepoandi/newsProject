099509dc3597cf43b18a170e995db0886a22058c
Tshepo <tshepo@localhost.localdomain>
2022-11-01 15:35:13 +0200

    navBar complete

---
+++ newsletter/src/components/NavBar/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useRef } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import '../../Styles/style.css'
+
+function Navbar() {
+  const navRef = useRef()
+  const showNavbar = () => {
+    navRef.current.classList.toggle('responsive_nav')
+  }
+  return (
+    <header>
+      <h3>The Biz</h3>
+      <nav ref={navRef}>
+        <a href="#">Headlines</a>
+        <a href="#">Search</a>
+        <a href="#">Messages</a>
+        <a href="#">Profile</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  )
+}
+
+export default Navbar
